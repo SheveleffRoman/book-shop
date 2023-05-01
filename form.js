5c6bd6f71faea0c53e926b173bec8d5c3e3a72c3
SheveleffRoman <118383206+SheveleffRoman@users.noreply.github.com>
2023-05-01 17:20:02 +0400

    Add files via upload

--- form.js
+++ form.js
@@ -18,26 +18,7 @@ getData().then(dataArray => {
     runCart();
 });
 
-function createHTMLTag(parentElement = null, tagName, attributes = {}, content = '', where = 'beforeend') {
-    const element = document.createElement(tagName);
-
-    // Добавляем произвольные атрибуты: id, class, etc
-    for (const key in attributes) {
-        element.setAttribute(key, attributes[key]);
-    }
-
-    // Добавляем произвольный контент
-    if (content !== '') {
-        element.innerHTML = content;
-    }
-
-    // Вставляем элемент после указанного родительского элемента
-    if (parentElement !== null) {
-        parentElement.insertAdjacentElement(where, element);
-    }
-
-    return element;
-}
+import {createHTMLTag} from "./createTag";
 
 // const form = document.querySelector('form');
 // const nameInput = form.querySelector('#name');
